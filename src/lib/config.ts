034700824cdc360aef337a25715fbefcb76946dd
NascentSpace <60989143+NascentSpace@users.noreply.github.com>
2025-06-11 00:05:20 +1000

    GodotChanger Test

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
+    CLIENT_ID: "1381987889998729307", // Discord Application ID
     COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
     PRODUCT_NAME: "Pong with GDScript", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
